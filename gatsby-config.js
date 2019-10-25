56344128703f26ed2873ab99b77e4bf6643fe826
Wren Turkal <wt@penguintechs.org>
2019-10-25 00:51:25 -0700

    Show correct format in quotes.toml header.
    Signed-off-by: Wren Turkal <wt@penguintechs.org>

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,9 +34,7 @@ module.exports = {
         path: `${__dirname}/data`,
       }
     },
-    {
-      resolve: `gatsby-transformer-toml`,
-    },
+    `gatsby-transformer-toml`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
