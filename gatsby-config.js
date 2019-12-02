d756eca16e5b0470f8be13bb92aa450c23988153
Wren Turkal <wt@penguintechs.org>
2019-12-01 23:11:19 -0800

    change manifest theme to match site theme

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,7 +24,7 @@ module.exports = {
         short_name: `ymatyt`,
         start_url: `/`,
         background_color: `white`,
-        theme_color: `mediumpurple`,
+        theme_color: `#9c27b0`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
